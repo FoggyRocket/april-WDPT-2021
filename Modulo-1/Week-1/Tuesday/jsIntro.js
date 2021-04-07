c712b7b20f24630e30ed33b75fa0d9b509a955f0
Dylan Torres <foggyrocket@MacBook-Pro-de-Dylan.local>
2021-04-06 20:43:44 -0500

    Day 1 js intro

---
+++ Modulo-1/Week-1/Tuesday/jsIntro.js
@@ -0,0 +1,36 @@
+
+var x = "Luis"; // Se puede modificar
+let b = `Yahir's house ""`; // esta si se puede modificar 
+const v = 'Esau';  // no se puede modificar
+
+let perro; 
+const miEdad = 28 // numero
+let status = true // 0 1 || true false boolean
+let novia = null 
+//console.log("Es el valor de x x con una ,¿¿¿¿", x)
+// #
+/*
+ksaodkosa
+MediaStreamAudioDestinationNodes
+SVGAnimatedString
+*/
+
+//Estructura de un for
+
+//for(let i= 0 ; i < 10 ; i ++)
+
+x = "Andres"
+b = '' 
+perro = 14902
+
+// {} objeto
+// [] arreglo
+// null
+
+console.log("Valores", perro)
+console.log("el status,",typeof status)
+/*
+if(novia === null || novia === undefined){
+  novia = 0
+} 
+*/
